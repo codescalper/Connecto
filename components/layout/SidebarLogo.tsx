906c5bf0cf7d240315e8c8df59f69c979889b3e9
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2023-07-05 06:30:24 +0000

    Added Tw logo

---
+++ components/layout/SidebarLogo.tsx
@@ -0,0 +1,25 @@
+import router, {useRouter} from "next/router";
+import { BsTwitter } from "react-icons/bs";
+
+
+
+const SidebarLogo = () =>{
+    return (
+        <div onClick={() =>router.push('/')}
+         className="
+        rounded-full
+        h-14
+        w-14
+        flex
+        items-center
+        justify-center
+        hover:bg-amber-500
+        hover:bg-opacity-10
+        cursor-pointer
+        transition
+        ">
+            <BsTwitter size={28} color="white" />
+        </div>
+    )
+}
+export default SidebarLogo
