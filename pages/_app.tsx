f3f1dc0a5e7d28c5054600963e922204fb05822e
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2023-07-06 04:02:41 +0000

    Created Register and Login Modal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import Layout from '@/components/Layout'
 import Modal from '@/components/Modal'
+import LoginModal from '@/components/modals/LoginModal'
+import RegisterModal from '@/components/modals/RegisterModal'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
@@ -7,6 +9,8 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
     {/* <Modal actionLabel='Submit' isOpen title="Test Modal" />  */} {/* Testing the modal component */}
+    <RegisterModal/>
+    <LoginModal />
     <Layout>
       <Component {...pageProps} />
     </Layout>
